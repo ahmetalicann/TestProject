26c9c13c83a01f4d5f78b5661cf98e6b4f110e6e
ahmettaliicann@gmail.com <ahmettaliicann@gmail.com>
2021-05-05 14:41:07 +0300

    Security İşlemleri devam

--- TestProject.MvcWebUI/Controllers/SecurityController.cs
+++ TestProject.MvcWebUI/Controllers/SecurityController.cs
@@ -30,7 +30,7 @@ namespace TestProject.MvcWebUI.Controllers
         }
 
         [HttpPost]
-        public async IActionResult Login(LoginViewModel loginViewModel, string returnUrl = "")
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel, string returnUrl = "")
         {
             //_userManager kullanıcıyı bulma confirmleri kontrol etme gibi işlemler yaparken _signInManager login işlemlerini gerçekleştiriyor.
             if (ModelState.IsValid)
@@ -82,7 +82,7 @@ namespace TestProject.MvcWebUI.Controllers
         {
             return View();
         }
-
+        [HttpPost]
         public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
         {
             if (ModelState.IsValid)
@@ -113,6 +113,89 @@ namespace TestProject.MvcWebUI.Controllers
         {
             TempData["email"] = email;
             return View();
+        }
+
+        //Mail onaylama methodumuz. User Id ve token gönderilir. user id bulunur. Daha sonrasında user ve token resulta atılır eğer başarılıysa Logine yönlendirilir.
+        public async Task<IActionResult> ConfirmEmail(string userId, string code)
+        {
+            if (userId == null || code == null)
+            {
+                RedirectToAction("Index", "Home");
+            }
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                throw new ApplicationException("Unable to find user!");
+            }
+            var result = await _userManager.ConfirmEmailAsync(user, code);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Login");
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel forgotPasswordViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(forgotPasswordViewModel);
+            }
+            var user = await _userManager.FindByEmailAsync(forgotPasswordViewModel.Email);
+            if (user == null)
+            {
+                return View(forgotPasswordViewModel);
+            }
+            var confirmationCode = await _userManager.GeneratePasswordResetTokenAsync(user);
+            var projectUrl = _configuration.GetSection("ProjectSettings").GetSection("ProjectUrl").Value;
+            var callBackUrl = projectUrl + Url.Action("ResetPassword", "Security", new { userId = user.Id, code = confirmationCode });
+
+            //send email
+
+            return RedirectToAction("ConfirmForgotPasswordInfo", new { email = user.Email });
+        }
+
+        public IActionResult ConfirmForgotPasswordInfo(string email)
+        {
+            TempData["email"] = email;
+            return View();
+        }
+
+        public IActionResult ResetPassword(string userId, string code)
+        {
+            if (userId == null || code == null)
+            {
+                throw new ApplicationException("User id or code must be supplied for password reset!");
+            }
+            var resetPasswordViewModel = new ResetPasswordViewModel
+            {
+                Code = code
+            };
+            return View(resetPasswordViewModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(ResetPasswordViewModel resetPasswordViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(resetPasswordViewModel.Email);
+                if (user == null)
+                {
+                    throw new ApplicationException("User not found!");
+                }
+                var result = await _userManager.ResetPasswordAsync(user, resetPasswordViewModel.Code, resetPasswordViewModel.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login");
+                }
+                return View(resetPasswordViewModel);
+            }
+            return View(resetPasswordViewModel);
         }
     }
 }
